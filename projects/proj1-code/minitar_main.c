fa5a41848dbdc0e5bdf91d238474158c93214300
yabdulle <abdul664@umn.edu>
2025-02-11 00:01:44 +0000

    Added comments to main

--- projects/proj1-code/minitar_main.c
+++ projects/proj1-code/minitar_main.c
@@ -18,10 +18,13 @@ int main(int argc, char **argv) {
     char *op = argv[1];
     char *archive_name = argv[3];
 
+    // Adding files to list
     for (int i = 4; i < argc; i++) {
         file_list_add(&files, argv[i]);
     }
 
+
+    // Create archive
     if (strcmp(op, "-c") == 0) {
         if (!files.head) {
             printf("Error: No files given to archive");
@@ -34,6 +37,7 @@ int main(int argc, char **argv) {
             return 1;
         }
 
+    // Append to archive
     } else if (strcmp(op, "-a") == 0) {
         if (!files.head) {
             printf("Error: No files provided");
@@ -46,6 +50,7 @@ int main(int argc, char **argv) {
             return 1;
         }
 
+    // List archive
     } else if (strcmp(op, "-t") == 0) {
         file_list_t archive_files;
         file_list_init(&archive_files);
@@ -64,6 +69,7 @@ int main(int argc, char **argv) {
         }
         file_list_clear(&archive_files);
 
+    // Update Archive
     } else if (strcmp(op, "-u") == 0) {
         if (!files.head) {
             printf("Error: No files provided");
@@ -71,6 +77,7 @@ int main(int argc, char **argv) {
             return 1;
         }
 
+        // Verify if file exists before updaiting
         FILE *update_file = fopen(archive_name, "r");
         if (update_file == NULL) {
             printf("Error: Archive file not present");
@@ -89,13 +96,14 @@ int main(int argc, char **argv) {
             return 1;
         }
 
+        // Verify if file already exists in archive
         if (!file_list_is_subset(&files, &archive_files)) {
             printf("Error: One or more of the specified files is not already present in archive\n");
             file_list_clear(&files);
             file_list_clear(&archive_files);
             return 1;
         }
-
+        // Finally append updated files
         if (append_files_to_archive(archive_name, &files) == -1) {
             printf("Error: Failed to update files");
             file_list_clear(&files);
@@ -106,14 +114,16 @@ int main(int argc, char **argv) {
         file_list_clear(&files);
         file_list_clear(&archive_files);
 
+    // Extract from archive
     } else if (strcmp(op, "-x") == 0) {
         if (extract_files_from_archive(archive_name) == -1) {
             printf("Error: Failed to extract files from archive");
             return 1;
         }
 
+    // Invalid command
     } else {
-        printf("Unknown command, try again");
+        printf("Unknown command");
         file_list_clear(&files);
         return 1;
     }
